23f0512e89d7a8c717163e9df23b3f23e9311a86
Samira-ABDI79 <samirabdiurmia@gmail.com>
2022-10-29 07:15:52 -0700

    feat (*) : add AddNotification

--- src/components/AddNotification/AddNotification.js
+++ src/components/AddNotification/AddNotification.js
@@ -1,14 +1,14 @@
 import {
     Button,
       FormControl,
-      FormLabel,
+  
       TextField,
       Typography,
       Box,
       Alert
     } from "@mui/material";
-    import React, { FormEvent, useState } from "react";
-    import { Link } from "react-router-dom";
+    import React, {  useState } from "react";
+  
     import {FormWrapper} from  "./AddNotificationStyle"
   
   
@@ -24,7 +24,7 @@ import {
       },
     };
     const buttonStyle={
-      textDecoration: "none",
+    
       color: "#fff",
       backgroundColor: "#26a69a",
       textAlign: "center",
@@ -37,122 +37,105 @@ import {
     }
   function AddNotification(){
    
-     const [enteredPassword, setEnteredPassword] = useState("");
-     const [passwordTouched, setPasswordTouched] = useState(false);
+     const [enteredText, setEnteredText] = useState("");
+     const [TextTouched, setTextTouched] = useState(false);
     
-     
-    //* password validation
-    let passwordErrorMessage = ["password should contain at least"];
-    let passwordIsValid = true;
-  
-    if (!/[a-zA-Z0-9]{6,}/.test(enteredPassword)) {
-      passwordErrorMessage.push(" 6 characters");
-      passwordIsValid = false;
-    }
-    if (!/(?=.*[A-Z])/.test(enteredPassword)) {
-      passwordErrorMessage.push(" one uppercase");
-      passwordIsValid = false;
-    }
-    if (!/(?=.*\d)/.test(enteredPassword)) {
-      passwordErrorMessage.push(" one digit");
-      passwordIsValid = false;
-    }
-    const passwordError = !passwordIsValid && passwordTouched;
-      const inputStyles = {
-          "& .MuiOutlinedInput-root": {
-            backgroundColor: "white",
-            height: { xs: "50px", md: "40px" },
-          },
-        };
-        const [enteredUsername, setEnteredUsername] = useState("");
-        const [usernameTouched, setUsernameTouched] = useState(false);
+   
+   
+        const [enteredTitle, setEnteredTitle] = useState("");
+        const [TitleTouched, setTitleTouched] = useState(false);
         
-    //* username validation
-    let usernameIsValid = true;
-    let usernameErrorMessage = "";
+    //* Title validation
+    let TitleIsValid = true;
+    let TitleErrorMessage = "";
   
-    if (!/^[a-zA-Z0-9]+$/.test(enteredUsername)) {
-      usernameErrorMessage = "Enter a valid username";
-      usernameIsValid = false;
+    if (!/^[a-zA-Z0-9]+$/.test(enteredTitle)) {
+      TitleErrorMessage = "Enter a valid Title";
+      TitleIsValid = false;
     }
-    if (enteredUsername.trim() === "") {
-      usernameErrorMessage = "Username is required";
-      usernameIsValid = false;
+    if (enteredTitle.trim() === "") {
+      TitleErrorMessage = "Title is required";
+      TitleIsValid = false;
     }
-    const usernameError = !usernameIsValid && usernameTouched;
+    const TitleError = !TitleIsValid && TitleTouched;
   
-    const user={
-      userName:"samira",
-      password:"samira#abdi"
-    }
+    
+    //* Text validation
+    let TextIsValid = true;
+    let TextErrorMessage = "";
   
-  const flag=(enteredUsername===user.userName && enteredPassword===user.password) ? true : false;
-   
-  const route=(flag ) ? "/panel/posts" : "/"
-  const alertFlag=((enteredUsername.trim() === "")||(enteredPassword.trim()==="")) ? true : false;
-   
+    if (!/^[a-zA-Z0-9]+$/.test(enteredText)) {
+      TextErrorMessage = "Enter a valid Text";
+      TextIsValid = false;
+    }
+    if (enteredText.trim() === "") {
+      TextErrorMessage = "Text is required";
+      TextIsValid = false;
+    }
+    const TextError = !TextIsValid && TextTouched;
+  
+    
     const submitHandler = (event) => {
+      // TextErrorMessage = "Text is required";
+
       event.preventDefault();
-      if (!passwordIsValid && !usernameIsValid) {
-        return;
+      if (enteredText.trim() === "" ) {
+        TextErrorMessage = "Text is required";
+        TextIsValid = false;
       }
+      if (enteredTitle.trim() === "") {
+        TitleErrorMessage = "Title is required";
+        TitleIsValid = false;
+      }
+    
     };
     return(
           <>
+           <form onSubmit={submitHandler}>
         <FormWrapper sx={{ display:"flex" ,flexDirection:"column", justifyContent:"space-around" , alignItems:"start" 
         , height:{lg:"300px"} , padding:"20px"
       }}>
          <FormControl fullWidth>
                   <TextField
                     variant="standard"
-                    label="Username"
-                    sx={usernameError ? inputErrorStyles : {}}
-                    value={enteredUsername}
-                    onChange={(e) => setEnteredUsername(e.target.value)}
-                    onBlur={() => setUsernameTouched(true)}
+                    label="Title"
+                    sx={TitleError ? inputErrorStyles : {}}
+                    value={enteredTitle}
+                    onChange={(e) => setEnteredTitle(e.target.value)}
+                    onBlur={() => setTitleTouched(true)}
                   />
-                  {usernameError && (
+                  {TitleError && (
                     <Typography
                       sx={{ color: "#f03637", fontSize: "14px", fontWeight: 500 }}
                     >
-                      {usernameErrorMessage}
+                      {TitleErrorMessage}
                     </Typography>
                   )}
                 </FormControl>
               <FormControl fullWidth>
                   <TextField
                     variant="standard"
-                    label="Password"
-                    type={"password"}
-                    sx={passwordError ? inputErrorStyles : {}}
-                    value={enteredPassword}
-                    onChange={(e) => setEnteredPassword(e.target.value)}
-                    onBlur={() => setPasswordTouched(true)}
+                    label="Text"
+                    
+                    sx={TextError ? inputErrorStyles : {}}
+                    value={enteredText}
+                    onChange={(e) => setEnteredText(e.target.value)}
+                    onBlur={() => setTextTouched(true)}
                   />
-                  {passwordError && (
+                  {TextError && (
                     <Typography
                       sx={{ color: "#f03637", fontSize: "14px", fontWeight: 500 }}
                     >
-                      {passwordErrorMessage.join(" ")}
+                      {TextErrorMessage.join(" ")}
                     </Typography>
                   )}
                 </FormControl>
-              <Button variant="contained" size="large" sx={buttonStyle}>
-             <Link to={route} style={buttonStyle}>Login </Link> 
+              <Button variant="contained" size="large" type={"submit"} sx={buttonStyle}>
+             افزودن 
               </Button>
-              {!alertFlag && !flag && (
-                 
-                   <Alert severity="error" >name and password is notvalid </Alert>
-  
-               
-                 )}
-                   {/* {flag && (
-              
-                   <Alert severity="success" >name and password isvalid </Alert>
-  
-            
-                 )} */}
+                  
           </FormWrapper>
+          </form>
           </>
   
       )
