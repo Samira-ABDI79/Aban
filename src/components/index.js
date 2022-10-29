ac417b9f6987babbe7b85ba47b09114dcd20cffb
Samira-ABDI79 <samirabdiurmia@gmail.com>
2022-10-29 04:03:54 -0700

    feat (*) : start task 3

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export * from "./Greeting/Greeting.jsx";
 // export  {default as CustomerRequest} from "./CustomerRequest/CustomerRequest"
 // export  {default as Inbox} from "./Inbox/Inbox"
 // export  { CustomerResponse} from "./CustomerResponse/CustomerResponse"
-export  {default as CommentsTable} from "./CommentsTable/CommentsTable"
+//export  {default as CommentsTable} from "./CommentsTable/CommentsTable"
 
 
 
